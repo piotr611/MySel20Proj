ec1ca36ebf7e42f255b44d3a3f6f86e64d71e21b
Piotr Gorbunov <piotrgorbunov@mail.ru>
2015-08-05 11:15:01 +0300

    5 morning 2

--- src/main/com/mysite/selenium/HomePaga.java
+++ src/main/com/mysite/selenium/HomePaga.java
@@ -1,8 +1,8 @@
 package com.mysite.selenium;
 
-//import org.openqa.selenium.By;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.*;
@@ -12,6 +12,10 @@ public class HomePaga {
 
         WebDriver driver = new FirefoxDriver();
         driver.get("http://google.com");
+
+        WebElement query = driver.findElement(By.name("q"));
+        query.sendKeys("hotel in Milane");
+
 
         System.out.println(driver.getTitle());
 
